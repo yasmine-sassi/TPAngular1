935d2f8d3570ab8d3c6d19e27ff4f150a9010781
alabenayed <alabenayed214@gmail.com>
2025-11-20 17:36:20 +0100

    products exercice

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import {
-  BehaviorSubject,
   Observable,
+  Subject,
   concatMap,
   map,
   takeWhile,
   scan,
+  startWith,
+  shareReplay,
 } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
@@ -17,8 +19,37 @@ import { Settings } from "./dto/product-settings.dto";
   styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent {
-  /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
-  /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
+  /* Flux déclaratif pour charger les produits page par page (12 par page) */
   products$!: Observable<Product[]>;
-  constructor() {}
+  hasMore$!: Observable<boolean>;
+  private loadMore$ = new Subject<void>();
+
+  constructor(private productService: ProductService) {
+    const page$ = this.loadMore$.pipe(
+      startWith(null),
+      // first emission -> page 0, then increment
+      scan((page) => (page === null ? 0 : (page as number) + 1), null as number | null),
+      map((p) => p as number)
+    );
+
+    const acc$ = page$.pipe(
+      concatMap((page) =>
+        this.productService.getProducts({ limit: 12, skip: page * 12 } as Settings)
+      ),
+      scan(
+        (acc, res) => ({ products: [...acc.products, ...res.products], total: res.total }),
+        { products: [] as Product[], total: 0 }
+      ),
+      // complete when we've loaded all products; include the final emission
+      takeWhile((acc) => acc.products.length < acc.total, true),
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+
+    this.products$ = acc$.pipe(map((acc) => acc.products));
+    this.hasMore$ = acc$.pipe(map((acc) => acc.products.length < acc.total));
+  }
+
+  loadMore() {
+    this.loadMore$.next();
+  }
 }
