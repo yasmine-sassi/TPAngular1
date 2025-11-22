758fa69eee15bd6e228736b91cfcade44e0d97ca
yasmine-sassi <yasmine.sassi@insat.ucar.tn>
2025-11-22 10:23:43 +0100

    changes

--- src/app/cv/masterdetailcv/masterdetailcv.component.ts
+++ src/app/cv/masterdetailcv/masterdetailcv.component.ts
@@ -5,21 +5,6 @@ import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-master-detail-cv',
   templateUrl: './masterdetailcv.component.html',
-  styleUrls: ['./masterdetailcv.component.css']
+  styleUrls: ['./masterdetailcv.component.css'],
 })
-export class MasterDetailCVComponent {
-  isDetailActive = false;
-
-  constructor(private router: Router) {
-    // Listen to route changes to show/hide the detail message
-    this.router.events
-      .pipe(
-        filter((event): event is NavigationEnd => event instanceof NavigationEnd)
-      )
-      .subscribe((event: NavigationEnd) => {
-        // Check if we're on a detail route (has ID in URL)
-        this.isDetailActive = event.urlAfterRedirects.includes('/cv/') && 
-                             !event.urlAfterRedirects.endsWith('/cv');
-      });
-  }
-}
+export class MasterDetailCVComponent {}
